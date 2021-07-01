4995204c608c8e611458a2254a2e41245b8fc8f8
Yonela Johannes <69614136+Yonela-Johannes@users.noreply.github.com>
2021-07-01 09:19:49 +0200

    First commit
    Who wants to be a Millionaire web app

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css';
+
+function App() {
+  const moneyPyramid = [
+    { id : 1, amount : "R 100" },
+    { id : 2, amount : "R 200" },
+    { id : 3, amount : "R 300" },
+    { id : 4, amount : "R 400" },
+    { id : 5, amount : "R 1 500" },
+    { id : 6, amount : "R 1 600" },
+    { id : 7, amount : "R 20 000" },
+    { id : 8, amount : "R 22 000" },
+    { id : 9, amount : "R 25 000"},
+    { id : 10, amount : "R 30 000" },
+    { id : 11, amount : "R 35 000" },
+    { id : 12, amount : "R 64 000" },
+    { id : 13, amount : "R 70 000" },
+    { id : 14, amount : "R 25 0000" },
+    { id : 15, amount : "R 1 000 000" },
+  ];
+
+  return (
+    <div className="app">
+      <div className="main">Main</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          {moneyPyramid.map((moneylist) => (
+
+            <li className="moneyListItem">
+            <span className="moneyListItemNumber">{moneylist.id}</span>
+            <span className="moneyListItemAmount">{moneylist.amount}</span>          
+          </li>
+        ))}
+        </ul>
+        </div>
+    </div>
+  );
+}
+
+export default App;
